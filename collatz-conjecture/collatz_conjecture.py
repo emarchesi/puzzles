b85297220c9dbfc933cbcbc53787f36827675864
Jun Xu <jxuclaire@gmail.com>
2019-09-18 16:32:36 +0000

    no changes

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,9 +1,13 @@
 
 def steps(number):
+    step = 0
     while number != 1:
+        
         if number % 2 == 1: 
             number = number*3 +1
-            step = n + 1
+            step = step + 1
         else:
             number = number/2
+            step = step+1
+    return step
     

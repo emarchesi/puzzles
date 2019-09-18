99a5c1f71b0da05295d6e4cc6030811bd4574ae5
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 20:17:38 +0000

    rotational_cipher finished

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -6,8 +6,8 @@ def rotate(text, key):
             i = ord(ch) + key 
             if i>ord('z'):
                 i-=26
-            finalletter=chr(i)
-            ciphertext+=finalletter
+            cipher_letter=chr(i)
+            ciphertext+=cipher_letter
     return ciphertext
 
 
